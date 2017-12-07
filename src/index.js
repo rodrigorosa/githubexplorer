52f700380828a8c21cc02f9fd57f4a0e16c227f1
Rodrigo Rosa <rodrigocrosa@gmail.com>
2017-12-07 21:36:02 -0200

    Store and reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import 'config/ReactotronConfig';
 
 import React from 'react';
-import Routes from './routes';
+import { Provider } from 'react-redux';
+import Routes from 'routes';
 
-const App = () => <Routes />;
+import store from 'redux/store';
+
+const App = () => (
+  <Provider store={store}>
+    <Routes />
+  </Provider>
+);
 
 export default App;
